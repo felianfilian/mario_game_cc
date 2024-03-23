455f8584e3e3d41364e8345abfc862c541c07c89
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-23 15:34:07 +0100

    create player class

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2D");
+
+class Player {
+  cosntructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    this.width = 100;
+    this.height = 100;
+  }
+}
