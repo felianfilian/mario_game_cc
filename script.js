22b2f68f028e174ff365a1b9b9004281ccb36b08
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-24 01:02:31 +0100

    create player object

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector("canvas");
-const c = canvas.getContext("2D");
+const c = canvas.getContext("2d");
 
 class Player {
-  cosntructor() {
+  constructor() {
     this.position = {
       x: 100,
       y: 100,
@@ -10,4 +10,11 @@ class Player {
     this.width = 100;
     this.height = 100;
   }
+
+  draw() {
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
 }
+
+const player = new Player();
+player.draw();
