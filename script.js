2b03d35ee5957f1c95982cc3c857fba7cf5f5f29
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-25 10:56:45 +0100

    jump logic

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ canvas.height = window.innerHeight;
 
 const gravity = 0.5;
 
+let jumpForce = 12;
+let isGrounded = true;
+let doubleJump = true;
+
 class Player {
   constructor() {
     this.position = {
@@ -30,10 +34,13 @@ class Player {
   update() {
     this.draw();
     this.position.y += this.velocity.y;
-    if (this.position.y + this.velocity.y + this.height <= canvas.height) {
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
       this.velocity.y += gravity;
+      isGrounded = false;
     } else {
       this.velocity.y = 0;
+      isGrounded = true;
+      doubleJump = true;
     }
   }
 }
@@ -46,3 +53,29 @@ function animate() {
 
 const player = new Player();
 animate();
+
+addEventListener("keydown", ({ keyCode }) => {
+  switch (keyCode) {
+    case 87: // up
+      jump();
+      break;
+    case 65: // left
+      break;
+    case 83: // down
+      break;
+    case 68: // right
+      break;
+    case 32: // space
+      jump();
+      break;
+  }
+});
+
+function jump() {
+  if (isGrounded) {
+    player.velocity.y -= jumpForce;
+  } else if (doubleJump) {
+    player.velocity.y -= jumpForce;
+    doubleJump = false;
+  }
+}
